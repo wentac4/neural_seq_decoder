fcd0cf93d3a94d70550f0841d6ed03d5f0db1ccd
lilightc <74816995+lilightc@users.noreply.github.com>
2025-12-05 22:42:06 -0800

    Add files via upload

---
+++ src/neural_decoder/neural_decoder_trainer_time_masking.py
@@ -0,0 +1,311 @@
+import os
+import pickle
+import time
+
+from edit_distance import SequenceMatcher
+import hydra
+import numpy as np
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader
+import torch.nn as nn
+
+from .model import GRUDecoder
+from .dataset import SpeechDataset
+
+
+def getDatasetLoaders(
+    datasetName,
+    batchSize,
+):
+    with open(datasetName, "rb") as handle:
+        loadedData = pickle.load(handle)
+
+    def _padding(batch):
+        X, y, X_lens, y_lens, days = zip(*batch)
+        X_padded = pad_sequence(X, batch_first=True, padding_value=0)
+        y_padded = pad_sequence(y, batch_first=True, padding_value=0)
+
+        return (
+            X_padded,
+            y_padded,
+            torch.stack(X_lens),
+            torch.stack(y_lens),
+            torch.stack(days),
+        )
+
+    train_ds = SpeechDataset(loadedData["train"], transform=None)
+    test_ds = SpeechDataset(loadedData["test"])
+
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=batchSize,
+        shuffle=True,
+        num_workers=0,
+        pin_memory=True,
+        collate_fn=_padding,
+    )
+    test_loader = DataLoader(
+        test_ds,
+        batch_size=batchSize,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=True,
+        collate_fn=_padding,
+    )
+
+    return train_loader, test_loader, loadedData
+
+################################################################################################
+class TimeMasking(nn.Module):
+    """
+    SpecAugment-style time masking for neural time series.
+    Randomly masks out consecutive time steps.
+    """
+    def __init__(self, max_mask_length=20, n_masks=2, mask_value=0.0, mask_noise_sd=0.0):
+        super().__init__()
+        self.max_mask_length = max_mask_length
+        self.n_masks = n_masks
+        self.mask_value = mask_value
+        self.mask_noise_sd = mask_noise_sd
+
+    def forward(self, x):
+        """
+        Args:
+            x: Input tensor (batch, time, channels) or (time, channels)
+        Returns:
+            Masked tensor with same shape
+        """
+        if self.training:  # Only apply during training
+            batch_dim = x.dim() == 3
+            if not batch_dim:
+                x = x.unsqueeze(0)
+
+            batch_size, time_steps, channels = x.shape
+
+            # Apply n_masks times
+            for _ in range(self.n_masks):
+                # Randomly choose mask length
+                mask_length = torch.randint(1, self.max_mask_length + 1, (1,)).item()
+                
+                # Randomly choose start position
+                max_start = max(1, time_steps - mask_length)
+                mask_start = torch.randint(0, max_start, (1,)).item()
+                
+                noise_shape = (batch_size, mask_length, channels)
+                # Draw noise from a Normal distribution N(0, mask_noise_sd)
+                noise = torch.randn(noise_shape, device="cuda") * self.mask_noise_sd + self.mask_value
+                # Apply mask
+                x[:, mask_start:mask_start + mask_length, :] = noise
+
+            if not batch_dim:
+                x = x.squeeze(0)
+
+        return x
+################################################################################################
+
+
+def trainModel(args):
+    os.makedirs(args["outputDir"], exist_ok=True)
+    torch.manual_seed(args["seed"])
+    np.random.seed(args["seed"])
+    device = "cuda"
+
+    with open(args["outputDir"] + "/args", "wb") as file:
+        pickle.dump(args, file)
+
+    trainLoader, testLoader, loadedData = getDatasetLoaders(
+        args["datasetPath"],
+        args["batchSize"],
+    )
+
+################################################################################################
+    n_masks=args.get("nMasks", 2)
+    if n_masks > 0:
+        time_masker = TimeMasking(
+            max_mask_length=args.get("maxMaskLength", 20),
+            n_masks=n_masks,
+            mask_value=0.0,
+            mask_noise_sd=args.get("mask_noise_sd", 0)
+        ).to(device)
+################################################################################################
+
+    model = GRUDecoder(
+        neural_dim=args["nInputFeatures"],
+        n_classes=args["nClasses"],
+        hidden_dim=args["nUnits"],
+        layer_dim=args["nLayers"],
+        nDays=len(loadedData["train"]),
+        dropout=args["dropout"],
+        device=device,
+        strideLen=args["strideLen"],
+        kernelLen=args["kernelLen"],
+        gaussianSmoothWidth=args["gaussianSmoothWidth"],
+        bidirectional=args["bidirectional"],
+    ).to(device)
+
+    loss_ctc = torch.nn.CTCLoss(blank=0, reduction="mean", zero_infinity=True)
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=args["lrStart"],
+        betas=(0.9, 0.999),
+        eps=0.1,
+        weight_decay=args["l2_decay"],
+    )
+    scheduler = torch.optim.lr_scheduler.LinearLR(
+        optimizer,
+        start_factor=1.0,
+        end_factor=args["lrEnd"] / args["lrStart"],
+        total_iters=args["nBatch"],
+    )
+
+    # --train--
+    testLoss = []
+    testCER = []
+    startTime = time.time()
+    for batch in range(args["nBatch"]):
+        model.train()
+
+        X, y, X_len, y_len, dayIdx = next(iter(trainLoader))
+        X, y, X_len, y_len, dayIdx = (
+            X.to(device),
+            y.to(device),
+            X_len.to(device),
+            y_len.to(device),
+            dayIdx.to(device),
+        )
+
+        # Noise augmentation is faster on GPU
+        if args["whiteNoiseSD"] > 0:
+            X += torch.randn(X.shape, device=device) * args["whiteNoiseSD"]
+
+        if args["constantOffsetSD"] > 0:
+            X += (
+                torch.randn([X.shape[0], 1, X.shape[2]], device=device)
+                * args["constantOffsetSD"]
+            )
+        
+################################################################################################
+        if n_masks > 0:
+            X = time_masker(X)
+################################################################################################
+
+        # Compute prediction error
+        pred = model.forward(X, dayIdx)
+
+        loss = loss_ctc(
+            torch.permute(pred.log_softmax(2), [1, 0, 2]),
+            y,
+            ((X_len - model.kernelLen) / model.strideLen).to(torch.int32),
+            y_len,
+        )
+        loss = torch.sum(loss)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+        # print(endTime - startTime)
+
+        # Eval
+        if batch % 100 == 0:
+            with torch.no_grad():
+                model.eval()
+                allLoss = []
+                total_edit_distance = 0
+                total_seq_length = 0
+                for X, y, X_len, y_len, testDayIdx in testLoader:
+                    X, y, X_len, y_len, testDayIdx = (
+                        X.to(device),
+                        y.to(device),
+                        X_len.to(device),
+                        y_len.to(device),
+                        testDayIdx.to(device),
+                    )
+
+                    pred = model.forward(X, testDayIdx)
+                    loss = loss_ctc(
+                        torch.permute(pred.log_softmax(2), [1, 0, 2]),
+                        y,
+                        ((X_len - model.kernelLen) / model.strideLen).to(torch.int32),
+                        y_len,
+                    )
+                    loss = torch.sum(loss)
+                    allLoss.append(loss.cpu().detach().numpy())
+
+                    adjustedLens = ((X_len - model.kernelLen) / model.strideLen).to(
+                        torch.int32
+                    )
+                    for iterIdx in range(pred.shape[0]):
+                        decodedSeq = torch.argmax(
+                            torch.tensor(pred[iterIdx, 0 : adjustedLens[iterIdx], :]),
+                            dim=-1,
+                        )  # [num_seq,]
+                        decodedSeq = torch.unique_consecutive(decodedSeq, dim=-1)
+                        decodedSeq = decodedSeq.cpu().detach().numpy()
+                        decodedSeq = np.array([i for i in decodedSeq if i != 0])
+
+                        trueSeq = np.array(
+                            y[iterIdx][0 : y_len[iterIdx]].cpu().detach()
+                        )
+
+                        matcher = SequenceMatcher(
+                            a=trueSeq.tolist(), b=decodedSeq.tolist()
+                        )
+                        total_edit_distance += matcher.distance()
+                        total_seq_length += len(trueSeq)
+
+                avgDayLoss = np.sum(allLoss) / len(testLoader)
+                cer = total_edit_distance / total_seq_length
+
+                endTime = time.time()
+                print(
+                    f"batch {batch}, ctc loss: {avgDayLoss:>7f}, cer: {cer:>7f}, time/batch: {(endTime - startTime)/100:>7.3f}"
+                )
+                startTime = time.time()
+
+            if len(testCER) > 0 and cer < np.min(testCER):
+                torch.save(model.state_dict(), args["outputDir"] + "/modelWeights")
+            testLoss.append(avgDayLoss)
+            testCER.append(cer)
+
+            tStats = {}
+            tStats["testLoss"] = np.array(testLoss)
+            tStats["testCER"] = np.array(testCER)
+
+            with open(args["outputDir"] + "/trainingStats", "wb") as file:
+                pickle.dump(tStats, file)
+
+
+def loadModel(modelDir, nInputLayers=24, device="cuda"):
+    modelWeightPath = modelDir + "/modelWeights"
+    with open(modelDir + "/args", "rb") as handle:
+        args = pickle.load(handle)
+
+    model = GRUDecoder(
+        neural_dim=args["nInputFeatures"],
+        n_classes=args["nClasses"],
+        hidden_dim=args["nUnits"],
+        layer_dim=args["nLayers"],
+        nDays=nInputLayers,
+        dropout=args["dropout"],
+        device=device,
+        strideLen=args["strideLen"],
+        kernelLen=args["kernelLen"],
+        gaussianSmoothWidth=args["gaussianSmoothWidth"],
+        bidirectional=args["bidirectional"],
+    ).to(device)
+
+    model.load_state_dict(torch.load(modelWeightPath, map_location=device))
+    return model
+
+
+@hydra.main(version_base="1.1", config_path="conf", config_name="config")
+def main(cfg):
+    cfg.outputDir = os.getcwd()
+    trainModel(cfg)
+
+if __name__ == "__main__":
+    main()
